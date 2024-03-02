016cb9df302f6dd5be753b88c55ed79ca5e01a41
Sai_Mohan_Ranees <126394616+SaiRanees@users.noreply.github.com>
2024-03-02 18:09:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,49 +7,49 @@ const searchInput = document.getElementById("search");
 const apiUrl = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
 async function fetchWeather(city) {
-  try {
-    const response = await fetch(apiUrl(city), { origin: "cors" });
-    const weatherData = await response.json();
-    console.log(weatherData);
-    displayWeather(weatherData);
-  } catch (error) {
-    console.error("Error fetching weather data:", error);
-  }
+    try {
+        const response = await fetch(apiUrl(city), { origin: "cors" });
+        const weatherData = await response.json();
+        console.log(weatherData);
+        displayWeather(weatherData);
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+    }
 }
 
 function displayWeather(data) {
-  const temperature = kelvinToCelsius(data.main.temp);
-  const humidity = data.main.humidity;
-  const windSpeed = data.wind.speed;
+    const temperature = kelvinToCelsius(data.main.temp);
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
 
-  const weatherDiv = document.createElement("div");
-  weatherDiv.classList.add("weather");
+    const weatherDiv = document.createElement("div");
+    weatherDiv.classList.add("weather");
 
-  weatherDiv.innerHTML = `
-    <h2>
-      <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>
-      ${temperature}°C
-      <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>
-    </h2>
-    <small>${data.weather[0].main}</small>
-    <div class="more-info">
-      <p>Humidity: <span>${humidity}%</span></p>
-      <p>Wind speed: <span>${Math.round(windSpeed * 3.6)} km/h</span></p>
-    </div>
-  `;
+    weatherDiv.innerHTML = `
+        <h2>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>
+            ${temperature}°C
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>
+        </h2>
+        <small>${data.weather[0].main}</small>
+        <div class="more-info">
+            <p>Humidity: <span>${humidity}%</span></p>
+            <p>Wind speed: <span>${Math.round(windSpeed * 3.6)} km/h</span></p>
+        </div>
+    `;
 
-  mainElement.innerHTML = "";
-  mainElement.appendChild(weatherDiv);
+    mainElement.innerHTML = "";
+    mainElement.appendChild(weatherDiv);
 }
 
 function kelvinToCelsius(kelvin) {
-  return Math.round(kelvin - 273.15);
+    return Math.round(kelvin - 273.15);
 }
 
 formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const city = searchInput.value.trim();
-  if (city) {
-    fetchWeather(city);
-  }
+    event.preventDefault();
+    const city = searchInput.value.trim();
+    if (city) {
+        fetchWeather(city);
+    }
 });
